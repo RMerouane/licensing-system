ffdabe320e15251ccb582d30f2eed7c05dac59d8
Merouane Rouibah <60047469+devMerouane@users.noreply.github.com>
2023-06-09 11:35:36 +0200

    setup typeorm with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root1234',
+      database: 'licensing-system',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [UserController],
   providers: [UserService],
 })
 export class AppModule {}
