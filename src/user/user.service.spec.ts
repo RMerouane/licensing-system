273e908b04f5eb931ec30f2b69d56cb2c354f5be
Merouane Rouibah <60047469+devMerouane@users.noreply.github.com>
2023-06-09 11:17:06 +0200

    feat: add the feature to create a user

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,5 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { BadRequestException } from '@nestjs/common';
+
+const mockedUser = {
+  userId: 1,
+  fullName: 'merouane',
+  email: 'rouibah.merouane@gmail.com',
+};
 
 describe('UserService', () => {
   let service: UserService;
@@ -14,5 +21,40 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create User', async () => {
+    const userPayload = {
+      fullName: 'merouane',
+      email: 'rouibah.merouane@gmail.com',
+      pwd: '123456789AZEIZEE',
+    };
+    const user = await service.createUser(userPayload);
+
+    expect(user).toEqual(mockedUser);
+  });
+
+  it('should return validation error if the password is not 12 characters ', async () => {
+    const userPayload = {
+      fullName: 'merouane',
+      email: 'rouibah.merouane@gmail.com',
+      pwd: '1234',
+    };
+
+    expect(() => service.createUser(userPayload)).toThrowError(
+      'password must have minimum 12 diffrents characters',
+    );
+  });
+
+  it("should throw validation error if the password doesn't have 12 diffrents characters", () => {
+    const userPayload = {
+      fullName: 'merouane',
+      email: 'rouibah.merouane@gmail.com',
+      pwd: 'azertyuiopqqqq',
+    };
+
+    expect(() => service.createUser(userPayload)).toThrowError(
+      'password must have minimum 12 diffrents characters',
+    );
   });
 });
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+
+export interface UserInput {
+  fullName: string;
+  email: string;
+  pwd: string;
+}
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  createUser(user: UserInput) {
+    const uniqueChracteres = new Set(user.pwd);
+
+    if (uniqueChracteres.size < 12) {
+      throw new Error('password must have minimum 12 diffrents characters');
+    }
+
+    if (user)
+      return {
+        userId: 1,
+        fullName: user.fullName,
+        email: user.email,
+      };
+  }
+}
